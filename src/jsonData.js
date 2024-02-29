df402282775e1c0706b169aa9c6b93a6edb3bc77
asad sabri <asad.sabri19@gmail.com>
2024-03-01 01:48:50 +0500

    fix the path of authentication

---
+++ src/jsonData.js
@@ -0,0 +1,21 @@
+export default [
+    {
+      id: "1",
+      UserName: "Asad",
+      type: "admin",
+      Password: "123456",
+    },
+    {
+      id: "2",
+      UserName: "zawar",
+      type: "subadmin",
+      Password: "1234567",
+    },
+    {
+      id: "3",
+      UserName: "sameer",
+      type: "customer",
+      Password: "12345678",
+    }
+  ];
+  
